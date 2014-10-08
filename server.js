3215145468cabccc7ddc77735a93d76fdaa42396
lwscroggins <larry.scroggins@gmail.com>
2014-10-08 15:01:14 -0700

    set up server and authorization. Need to work on angular view for authorization page.

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var express = require('express');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var http = require('http');
+var passport = require('passport');
+var app = express();
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/GolfScorecard');
+
+app.use(express.static(__dirname + (process.env.STATIC_DIR || '/build')));
+
+app.set('jwttokensecret', process.env.JWT_SECRET || 'developmentsecret');
+app.set('secret', process.env.SECRET || 'developmentsecret');
+
+app.use(passport.initialize());
+
+require('./lib/passport')(passport);
+var jwtauth = require('./lib/jwtauth')(app);
+
+app.use(bodyparser.json());
+
+var server = http.createServer();
+
+app.set('port', process.env.PORT || 3000);
+
+server.listen(app.get('port'), function() {
+  console.log('server is listening on port: ', app.get('port'));
+});
